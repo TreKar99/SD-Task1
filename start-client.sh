fad2a2822e37abc419a9ead7c501ede55f457991
TreKar99 <101529212+TreKar99@users.noreply.github.com>
2024-04-10 09:50:43 +0000

    colorcitos

--- start-client.sh
+++ start-client.sh
@@ -24,27 +24,17 @@ while true; do
 
   case $choice in
     1)
-      # Implement logic for connecting to a chat (private or group)
-      echo "Option 1: Connect to chat"
-      # Replace this with your actual implementation for connecting to a chat
-      # It should prompt for chat ID and handle private/group chat scenarios
-      # You might need to use tools like 'curl' or 'wget' to communicate
-      # with the chat server
+      echo -e "\033[31mOption 1: Connect to chat\033[0m"
       ;;
     2)
-      # Implement logic for subscribing to a group chat
-      echo "Option 2: Subscribe to group chat"
-      # Replace this with your actual implementation for subscribing to a group chat
-      # It should prompt for chat ID and handle creating the chat if non-existent
-      # You might need to use tools like 'curl' or 'wget' to communicate
-      # with the chat server
+      echo -e "\033[32mOption 2: Subscribe to group chat\033[0m"
       ;;
     3)
-      echo "Option 3: Discover chats"
+      echo -e "\033[33mOption 3: Discover chats\033[0m"
       python3 Redis/redisNameServer.py
       ;;
     4)
-      echo "Option 4: Access insult channel"
+      echo -e "\033[34mOption 4: Access insult channel\033[0m"
       python3 RabbitQueue/insultChannel.py      
       ;;
     5)
